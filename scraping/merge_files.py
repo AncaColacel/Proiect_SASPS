ff03b063ac0f39902b0153c0f7a82eee8dca7181
Colacel Anca-Maria <94076842+AncaColacel@users.noreply.github.com>
2025-11-14 14:13:37 +0200

    create script fro extract topics from news

--- scraping/merge_files.py
+++ scraping/merge_files.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-folder = "../jsons"  
+folder = "../jsons/topics/"  
 
 combined = []
 
@@ -13,7 +13,8 @@ for filename in os.listdir(folder):
             data = json.load(f)
             combined.append(data)   # păstrează TOT fișierul, metadate + articole
 
-with open("../jsons/merged.json", "w", encoding="utf-8") as out:
+with open("../jsons/baza_date_with_topics.json", "w", encoding="utf-8") as out:
     json.dump(combined, out, ensure_ascii=False, indent=2)
 
-print("✔️ Fișierele au fost unite în merged.json păstrând metadatele.")
+print("✔️ Fișierele au fost unite în baza_date_with_topics.json păstrând metadatele.")
+    
